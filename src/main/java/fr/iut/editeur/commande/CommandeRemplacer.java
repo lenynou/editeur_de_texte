e8fde36a67d92362b9fb85bb42f3cbe30a25f9eb
soulard <leny.soulard@etu.umontpellier.fr>
2023-11-07 16:46:15 +0100

    modifie bug commande remplacer

--- src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -18,7 +18,7 @@ public class CommandeRemplacer extends CommandeDocument {
         int debut = Integer.parseInt(parameters[1]); //index debut
         int fin = Integer.parseInt(parameters[2]); //index fin
         String texte = parameters[3]; //texte
-        this.document.ajouter(texte);
+        this.document.remplacer(debut, fin , texte);
         super.executer();
     }
 
