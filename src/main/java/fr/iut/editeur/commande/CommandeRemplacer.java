19b78d2091c934d9b2d7e197c875fe9742de00d2
soulard <leny.soulard@etu.umontpellier.fr>
2023-11-07 16:36:52 +0100

    ajout methode remplacer
    ajout commande remplacer
    
    correction de la methode executer de la commande remplacer
    
    ajout commentaire
    
    ajout commande remplacer dans CommandeFactory
    
    modifie bug commande remplacer
    
    Ajout commande Remplacer

---
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -0,0 +1,25 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument {
+
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if(parameters.length < 2) {
+            System.err.println("Format attendu : remplacer;debut;fin;texte");
+            return;
+        }
+        int debut = Integer.parseInt(parameters[1]); //index debut
+        int fin = Integer.parseInt(parameters[2]); //index fin
+        String texte = parameters[3]; //texte
+        this.document.remplacer(debut, fin , texte);
+        super.executer();
+    }
+
+}
